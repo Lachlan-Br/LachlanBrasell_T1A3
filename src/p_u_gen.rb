0d8545db96b7aec60366a74240eedf3fc52f1db6
Lachlan-Br <gcab022113@coderacademy.edu.au>
2021-12-11 13:51:20 +1000

    test file for password issues

--- src/p_u_gen.rb
+++ src/p_u_gen.rb
@@ -2,3 +2,23 @@
 >> require 'passgen'
 >> Passgen::generate
 => "zLWCeS3xC9"
+
+
+
+will do:
+    - methods to do uppercase and lower
+        -do only lower
+        -numbers and  symbols
+        
+    - methods to do usernames similar to above but
+        - only upper and lower case
+        - no symbols
+        - number and letters only.
+        
+    end
+
+
+
+user_length = gets.chomp.to_i
+
+Passgen::generate(:length => user_length)
