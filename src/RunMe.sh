d96a7b4d0709bc7e1393a2c775e475422cf2c7c9
Lachlan-Br <gcab022113@coderacademy.edu.au>
2021-12-11 13:11:57 +1000

    First commit

---
+++ src/RunMe.sh
@@ -0,0 +1,16 @@
+can do arguments on this line (echo $1 / echo $2 etc)
+
+rm Gemfile.lock
+
+gem install bundle
+
+clear
+
+ruby index.rb ($1 if you want arugment from first line.)
+
+
+
+
+
+Highly recommend adding the script command in your HELP file in README
+ (this is considered good practice, the more help the better), for other users to test
