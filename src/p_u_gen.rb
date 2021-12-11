bb3d65e85ed1db33a7b31e7717806e9917ef6b45
Lachlan-Br <gcab022113@coderacademy.edu.au>
2021-12-11 14:10:37 +1000

    password gen is setup in case gem failure

--- src/p_u_gen.rb
+++ src/p_u_gen.rb
@@ -22,3 +22,44 @@ will do:
 user_length = gets.chomp.to_i
 
 Passgen::generate(:length => user_length)
+
+
+
+
+
+
+
+
+if passgen does not work can use - This allows users to choose a length.
+
+#This is for without symbols
+    CHARS_LNC = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
+    def user_pass_gen (length = gets.chomp.to_i)
+    CHARS_LN.sort_by { rand }.join[0...length]
+    end
+    
+    puts "This is your new password: " + user_pass_gen
+
+#this is for with symbols
+    CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a + ('!'..'+').to_a
+    def user_pass_gen (length = gets.chomp.to_i)
+    CHARS.sort_by { rand }.join[0...length]
+    end
+    
+    puts "This is your new password: " + user_pass_gen
+
+#This is for with symbols but no caps
+CHAR_LNS = ('0'..'9').to_a + ('a'..'z').to_a + ('!'..'+').to_a
+    def user_pass_gen (length = gets.chomp.to_i)
+    CHARS.sort_by { rand }.join[0...length]
+    end
+    
+    puts "This is your new password: " + user_pass_gen
+
+#This is for no numbers
+CHARS_LSC = ('A'..'Z').to_a + ('a'..'z').to_a + ('!'..'+').to_a
+    def user_pass_gen (length = gets.chomp.to_i)
+    CHARS.sort_by { rand }.join[0...length]
+    end
+    
+    puts "This is your new password: " + user_pass_gen
--- src/testing.rb
+++ src/testing.rb
@@ -1,2 +1,9 @@
-Passgen::generate
-Passgen::generate(:length => 20)
+
+
+
+CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a + ('!'..'+').to_a
+def user_pass_gen (length = gets.chomp.to_i)
+CHARS.sort_by { rand }.join[0...length]
+end
+
+puts "This is your new password: " + user_pass_gen
